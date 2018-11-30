821f7ebb91079aca25924acad47a0cd101fd57d5
Akash Arora <arora110@mail.chapman.edu>
2018-11-30 00:13:22 -0800

    Add files via upload
    'prev' displays previous equation, exponentiation added, and better program/function separation.

--- calcProto.f90
+++ calcProto.f90
@@ -2,68 +2,45 @@ program input
 
 implicit none
 
-! variables whose names start with I,J,K,L,M or N are implicitly INTEGER
-! so naming character :: input throws type error. so weird.
-
-! implicit none should disable this implicit type-casting, but somehow its not…
-
-character*20 :: equation ! ONLY 20 FOR BIG NUMBERS THIS WILL MIGHT ERROR
+character*40 :: equation
 integer :: eqLen
 integer :: first_num
 character*1 :: operation
 integer :: second_num
 integer :: ans
 
-
 character*10 :: eqPartOne, eqPartTwo
-integer :: i
+integer :: i ! for loops, maybe get rid of and make local if possible but this for now
 
-write (*,*)
-write (*,*)
-write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-write (*,*) "By Akash Arora"
-write (*,*) "CPSC 354, Kurz"
-write (*,*) "Fall 2019"
-write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-write (*,*)
-write (*,*) "Welcome to Fortulator!"
-write (*,*)
-write (*,*) "The best dam fortran command-line calculator at Chapman"
-write (*,*) 
-write (*,*) 
-write (*,*) "some helpful commands: " 
-write (*,*) "     ’op’ to see available operations [WORKING]"
-write (*,*) "     ’prev’ to see your last calculation [nA]"
-write (*,*) "     ’ans’ to use the previous answer in your current equation. (‘ans + 5’) [WORKING]"
-write (*,*) "     ’exit’ to quit [WORKING]" 
-write (*,*)
+integer :: endOfLogIndex
+integer :: prevCount
 
-write (*,*) "go ahead! type in something like '11 + 4' and press [Enter]..."
-write (*,*) "note: currently not accepting negatives numbers in input"
-write (*,*)
+integer :: test
+character*30 :: prevEquation
 
+! List of available operation types to validate against
+Character (len=65), Allocatable :: opTypes(:)
+opTypes = [Character(len=65) :: "+","-","*","/", "^"]
+
+ans = printIntro(1,1) ! cleaner but fix this assignment, should just call printIntro and other functions
 ans = 0
 
-do while (equation /= 'exit')
+do ! infinte loop
 	read (*,"(A)") equation
-	if (equation == 'op') then
-		write (*,*)
-		write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-		write (*,*) "available operations: " 
-		write (*,*) "     ’+’ // Addition          // '4+2'"       
-		write (*,*) "     ’-’ // Subtraction       // '6-3'"
-		write (*,*) "     ’*’ // Multiplication    // '7*3'"       
-		write (*,*) "     ’/’ // Division          // '4/2'"
-		write (*,*)
-		write (*,*) "note: division currently rounds down"
-		write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	if (equation == 'exit') then
+		exit
+	else if (equation == 'op') then
+		ans = printOperations(1,1)
+		ans = 0
+	else if (equation == 'prev') then
+		write (*,*) prevEquation
 		write (*,*)
 	else if (equation /= 'exit') then ! clean this up w/ infinite while loop, if 'exit' break else if…
 		eqLen = len_trim(equation)
 		! NOTE: still need to trim white spaces before and after operation, cleaner UI
 		do i=1, eqLen
-			if (equation(i:i) == '+'.or.equation(i:i) == '-'.or.equation(i:i) == '*'.or.equation(i:i) == '/') then
-				!write (*,*) "Inside IF"
+			if (ANY(opTypes == equation(i:i))) then
+				! write (*,*) "Inside IF"
 				eqPartOne = equation(1:i-1)
 				eqPartOne = eqPartOne(1:len_trim(eqPartOne))
 				if (eqPartOne == 'ans') then
@@ -83,26 +60,25 @@ do while (equation /= 'exit')
 				end if
 			end if
 		end do
+
 		ans = calc(first_num, second_num, operation)
+	
+		! save equation / ans
+		prevEquation = storeEquation(first_num, operation, second_num, ans)
+
 		write(*,*) "= ", ans
 		write(*,*)
 	end if
 end do
 
-write (*,*)
-write (*,*)
-write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-write (*,*) "               Hope you enjoyed using Fortulator!"
-write (*,*) "      the best dam fortran command-line calculator at Chapman" 
-write (*,*) 
-write (*,*) "By Akash Arora"
-write (*,*) "CPSC 354, Kurz"
-write (*,*) "Fall 2019"
-write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-write (*,*)
+ans = printExitMessage(1,1)
+ans = 0
  
 contains
 
+! FUNCTIONS! FUNCTIONS! FUNCTIONS! FUNCTIONS! FUNCTIONS! FUNCTIONS!
+
+! This function CALCULATES THE EQUATION
 function calc(a, b, op) result(j)
 	implicit none
 	integer, intent(in) :: a, b
@@ -116,7 +92,110 @@ function calc(a, b, op) result(j)
     		j = a * b
 	else if (op == '/') then
     		j = a / b
+	else if (op == "^") then
+		j = a ** b
 	end if
 end function calc
+
+! This function CONVERTS EQUATION PARTS INTO A STRING FOR 'prevEquation
+function storeEquation(first_num, op, second_num, ans) result(equation)
+	implicit none
+	integer, intent	(in) :: first_num, second_num, ans
+	character*1, intent(in) :: op
+	
+	character*10 first_num_str
+	character*10 second_num_str
+	character*10 ans_str
+	character*20 equation
+
+	character*1 space
+	character*3 equals
+
+	space = " "
+	equals = " = "
+
+	write(first_num_str, '(i10)') first_num
+	write(first_num_str, *) trim(adjustl(first_num_str))
+
+	write(second_num_str, '(i10)') second_num
+	write(second_num_str, *) trim(adjustl(second_num_str))
+
+	write(ans_str, '(i10)') ans
+	write(ans_str, *) trim(adjustl(ans_str))
+	
+	equation = trim(first_num_str) // space // trim(op) // space // trim(second_num_str) // equals // trim(ans_str)
+end function storeEquation
+
+! This function PRINTS INTRO MESSAGE
+function printIntro(a, b) result(j)
+	implicit none
+	integer, intent(in) :: a, b
+	integer :: j
+	j = 1
+	write (*,*)
+	write (*,*)
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*) "By Akash Arora"
+	write (*,*) "CPSC 354, Kurz"
+	write (*,*) "Fall 2019"
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*)
+	write (*,*) "Welcome to Fortulator!"
+	write (*,*)
+	write (*,*) "The best dam fortran command-line calculator at Chapman"
+	write (*,*) 
+	write (*,*) 
+	write (*,*) "some helpful commands: " 
+	write (*,*) "     ’op’ to see available operations [WORKING]"
+	write (*,*) "     ’prev’ to see your last calculation [WORKING]"
+	write (*,*) "     ’ans’ to use the previous answer in your current equation. (‘ans + 5’) [WORKING]"
+	write (*,*) "     ’exit’ to quit [WORKING]" 
+	write (*,*)
+
+	write (*,*) "go ahead! type in something like '11 + 4' and press [Enter]..."
+	write (*,*) "note: currently not accepting negatives numbers in input"
+	write (*,*)
+end function printIntro
+
+
+! This function PRINTS AVAILABLE OPERATIONS
+function printOperations(a,b) result(j)
+	implicit none
+	integer, intent(in) :: a, b
+	integer :: j
+	j = 1
+	write (*,*)
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*) "available operations: " 
+	write (*,*) "     ’+’ // Addition          // '4+2'"       
+	write (*,*) "     ’-’ // Subtraction       // '6-3'"
+	write (*,*) "     ’*’ // Multiplication    // '7*3'"       
+	write (*,*) "     ’/’ // Division          // '4/2'"
+	write (*,*) "     ’^’ // Exponentiation    // '3^3'"
+	write (*,*)
+	write (*,*) "note: division currently rounds down"
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*)
+end function printOperations
+
+
+! This function PRINTS EXIT MESSAGE
+function printExitMessage(a,b) result(j)
+	implicit none
+	integer, intent(in) :: a, b
+	integer :: j
+	j = 1
+	write (*,*)
+	write (*,*)
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*) "               Hope you enjoyed using Fortulator!"
+	write (*,*) "      the best dam fortran command-line calculator at Chapman" 
+	write (*,*) 
+	write (*,*) "By Akash Arora"
+	write (*,*) "CPSC 354, Kurz"
+	write (*,*) "Fall 2019"
+	write (*,*) "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	write (*,*)
+end function printExitMessage
 
 end program input
